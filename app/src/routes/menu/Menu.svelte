69779672ff0c95092f59792a2385bf80990a09ef
SnipUndercover <snipundercover@gmail.com>
2022-05-11 10:40:54 +0200

    Create working menu

---
+++ app/src/routes/menu/Menu.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import { Link } from 'svelte-navigator';
+  import { cubicOut } from 'svelte/easing'
+  import { fade, fly } from 'svelte/transition'
+  import Button from './lib/Button.svelte';
+
+  const buttons = [
+    {
+      title: "VS CPU",
+      to: "cpu",
+      id: 1
+    },
+    {
+      title: "Two players (offline)",
+      to: "offline",
+      id: 2
+    },
+    {
+      title: "Two players (online)",
+      to: "online",
+      id: 3
+    }
+  ]
+</script>
+
+<main out:fade>
+  <header in:fly="{{ y: -25, duration: 2000, easing: cubicOut }}">
+    <h1>Welcome to Pong!</h1>
+    <h3>Select a mode below:</h3>
+  </header>
+  <article>
+    {#each buttons as button}
+      <Link to={button.to} style="text-decoration: none">
+        <Button title={button.title} id={button.id}/>
+      </Link>
+    {/each}
+  </article>
+</main>
+
+<style lang="sass">
+  main
+    width: 100vw
+    height: 100vh
+    
+    display: flex
+    align-items: center
+    flex-direction: column
+    justify-content: space-around
+
+    header
+      text-align: center
+
+      h1:focus
+        outline: none
+
+    article
+      display: flex
+      flex-direction: column
+      justify-content: center
+      gap: 2vh
+</style>
---
+++ app/src/routes/menu/lib/Button.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { fly } from 'svelte/transition';
+  import { cubicOut } from "svelte/easing";
+
+  export let title: string;
+  export let id: number;
+  
+</script>
+
+<div in:fly="{{ 
+  y: 25,
+  duration: 2000,
+  delay: id * 125,
+  easing: cubicOut
+}}">
+  {title}
+</div>
+
+<style lang="sass">
+  div
+    background-color: #ccc
+    color: black
+    
+    font-size: 24px
+    
+    border-radius: 5px
+    padding: 0.5em
+    text-align: center
+    text-decoration: none
+</style>
