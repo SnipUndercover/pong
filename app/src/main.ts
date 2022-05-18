ed1eb91073714bb20b01ecdfc69731c3c8bf9fc6
SnipUndercover <snipundercover@gmail.com>
2022-05-18 16:55:51 +0200

    Assert `div#app` to always be present

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,7 +2,7 @@ import App from "./App.svelte";
 import "./assets/scss/style.scss";
 
 const app = new App({
-  target: document.getElementById("app")
+  target: document.getElementById("app") as HTMLDivElement // will always be present
 });
 
 export default app;
