eb088bbb0a4181769e01c1635614f11baf04975b
SnipUndercover <snipundercover@gmail.com>
2022-05-28 20:21:06 +0200

    Warn if the end user is on a mobile device

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -2,8 +2,10 @@
   import { Router, Route } from "svelte-navigator";
   import Menu from "./routes/menu/Menu.svelte";
   import Todo from "./Todo.svelte";
+  import MobileWarning from "./routes/menu/MobileWarning.svelte";
   import routes from "./routes";
   import Board, { HandlerType } from "./routes/play/Board.svelte";
+  import mobile from 'is-mobile';
 </script>
 
 <Router primary={false} basepath="/">
@@ -11,10 +13,18 @@
     <Menu />
   </Route>
   <Route path={routes[0].to}>
-    <Board handler={HandlerType.CPU} />
+    {#if mobile()} 
+      <MobileWarning />
+    {:else}
+      <Board handler={HandlerType.CPU} />
+    {/if}
   </Route>
   <Route path={routes[1].to}>
-    <Board />
+    {#if mobile()} 
+      <MobileWarning />
+    {:else}
+      <Board />
+    {/if}
   </Route>
   <Route path={routes[2].to}>
     <Todo text="Implement online gameplay" />
---
+++ app/src/routes/menu/MobileWarning.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+</script>
+
+<div transition:fade>
+  <h1>
+    Sorry,<br />
+    this application<br />
+    does not support<br />
+    mobile devices<br />
+    yet.
+  </h1>
+  <h3>but it may in the future...</h3>
+</div>
+
+<style lang="scss">
+  div {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    h1:focus {
+      outline: none;
+    }
+    h3 {
+      opacity: 25%;
+    }
+  }
+</style>
